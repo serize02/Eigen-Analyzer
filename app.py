359b0e02b50f9f6d99512f55b310e7463fb764a5
serize02 <ernestoserize@gmail.com>
2025-01-20 11:03:55 -0500

    ref: app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from streamlit_lottie import st_lottie
 from methods import Methods
 from load import Load
 
-def analyze_single_matrix(matrix):
+def analyze_single_matrix(matrix, results):
     st.write("""
         ## Error Reduction on Estimation
     """)
@@ -41,118 +41,121 @@ def analyze_single_matrix(matrix):
         st.latex(r"\lambda_0 \approx" + str(results['power-method-values'][0][-1]))
         st.latex(r"\lambda_1 \approx" + str(results['inverse-power-method-values'][0][-1]))
 
-st.set_page_config(layout="wide")
 
-homepage = st.empty()
+def main():
+    st.set_page_config(layout="wide")
 
-example_dataset = pd.read_csv('assets/dataset.csv')
+    homepage = st.empty()
 
-with homepage:
+    with homepage:
 
-    text, img = st.columns((3, 1))
+        text, img = st.columns((3, 1))
 
-    with text:
-        text.write("""
-        # Welcome to Eigen-Analyzer
-        Eigen-Analyzer is a powerful yet intuitive tool designed to explore the fascinating world of matrices. Created with educational research in mind, this app helps you dive deeper into the mathematical concepts of eigenvalues and eigenvectors.
-        At its core, Eigen-Analyzer harnesses the Power Method and its variant, the Inverse Power Method, two fundamental algorithms taught in Numerical Analysis. These methods drive the calculations and provide insights into matrix behavior, making this app an excellent companion for students mastering these topics.
-        
-        Whether you’re a student, educator, or math enthusiast, Eigen-Analyzer is here to enhance your understanding of linear algebra and numerical analysis, making your exploration both insightful and engaging.
-        
-        ##### Discover the elegance of mathematics with Eigen-Analyzer!
-        
-        ## How to Use
-        1. **Upload a CSV file**: The file should contain a column named `matrix` with the matrices to be analyzed.
-        2. **Set Parameters**: Use the sidebar to set the tolerance and the number of iterations.
-        3. **Analyze**: Once the file is uploaded, the analysis will start automatically.
-        """)
+        with text:
+            text.write("""
+            # Welcome to Eigen-Analyzer
+            Eigen-Analyzer is a powerful yet intuitive tool designed to explore the fascinating world of matrices. Created with educational research in mind, this app helps you dive deeper into the mathematical concepts of eigenvalues and eigenvectors.
+            At its core, Eigen-Analyzer harnesses the Power Method and its variant, the Inverse Power Method, two fundamental algorithms taught in Numerical Analysis. These methods drive the calculations and provide insights into matrix behavior, making this app an excellent companion for students mastering these topics.
+            
+            Whether you’re a student, educator, or math enthusiast, Eigen-Analyzer is here to enhance your understanding of linear algebra and numerical analysis, making your exploration both insightful and engaging.
+            
+            ##### Discover the elegance of mathematics with Eigen-Analyzer!
+            
+            ## How to Use
+            1. **Upload a CSV file**: The file should contain a column named `matrix` with the matrices to be analyzed.
+            2. **Set Parameters**: Use the sidebar to set the tolerance and the number of iterations.
+            3. **Analyze**: Once the file is uploaded, the analysis will start automatically.
+            """)
 
-    with img:
-        animation = Load.load_lottiefile("assets/animation.json")
-        st_lottie(animation, height=350, width=300)
+        with img:
+            animation = Load.load_lottiefile("assets/animation.json")
+            st_lottie(animation, height=350, width=300)
 
-st.sidebar.title("Parameters")
-tolerance = st.sidebar.number_input("Tdolerance", value=1e-6, format="%.6f", step=1e-6)
-iterations = int(st.sidebar.slider("Number of Iterations",min_value=1,max_value=100,value=10,step=1))
-file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+    st.sidebar.title("Parameters")
+    tolerance = st.sidebar.number_input("Tolerance", value=1e-6, format="%.6f", step=1e-6)
+    iterations = int(st.sidebar.slider("Number of Iterations",min_value=1,max_value=100,value=10,step=1))
+    file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
 
-results = None
+    results = None
 
-if file is not None:
-    df = pd.read_csv(file)
-    if 'matrix' not in df.columns:
-        st.error("Please upload a valid CSV file.")
-    else:
-        valid = True
-        for index, row in df.iterrows():
-            try:
-                matrix = np.array(eval(row['matrix']))
-            except:
-                valid = False
-                break
-        if not valid:
-            st.error("The matrix column contains invalid data.")
+    if file is not None:
+        df = pd.read_csv(file)
+        if 'matrix' not in df.columns:
+            st.error("Please upload a valid CSV file.")
         else:
-            with st.sidebar, st.spinner("Analyzing data..."):
-                time.sleep(3)
-                results = Methods.analyze_data(df, tolerance, iterations)
+            valid = True
+            for index, row in df.iterrows():
+                try:
+                    matrix = np.array(eval(row['matrix']))
+                except:
+                    valid = False
+                    break
+            if not valid:
+                st.error("The matrix column contains invalid data.")
+            else:
+                with st.sidebar, st.spinner("Analyzing data..."):
+                    time.sleep(3)
+                    results = Methods.analyze_data(df, tolerance, iterations)
 
 
 
-if results is not None:
+    if results is not None:
 
-    if len(results) == 0:
-        st.error("No valid matrices were found in the dataset.")
-        st.stop()
+        if len(results) == 0:
+            st.error("No valid matrices were found in the dataset.")
+            st.stop()
 
-    homepage.empty()
+        homepage.empty()
 
-    if len(results) == 1:
-        matrix = np.array(eval(results['matrix'][0]))
-        analyze_single_matrix(matrix)
+        if len(results) == 1:
+            matrix = np.array(eval(results['matrix'][0]))
+            analyze_single_matrix(matrix, results)
 
 
-    else :
-        dominant = [v[len(v) - 1] for v in results['power-method-values']]
-        results['power-method-iterations'] = [len(v) for v in results['power-method-values']]
-        results['inverse-power-method-iterations'] = [len(v) for v in results['inverse-power-method-values']]
+        else :
+            dominant = [v[len(v) - 1] for v in results['power-method-values']]
+            results['power-method-iterations'] = [len(v) for v in results['power-method-values']]
+            results['inverse-power-method-iterations'] = [len(v) for v in results['inverse-power-method-values']]
 
-        st.write("""
-            ## Summary Statistics
-            ##### The summary statistics provide an overview of the data distribution and convergence behavior.
-        """)
-
-        col1, col2 = st.columns([0.75,2.75])
-
-        with col1:
-            st.write(results.describe())
-        with col2:
-            st.write(results.head(8))
-
-        st.write("""
-            ## Dominant Eigenvalues
-        """)
-        st.scatter_chart(dominant, x_label='Matrix-Index', height=500)
-
-        st.write("""
-            ## Number of Iterations Required to Converge to the Dominant Eigenvalue
-            ##### The Inverse Power Method is a modification of the Power Method that gives faster convergence. It is not common to see the number of iterations required to converge for the Power Method to be less than the Inverse Power Method.
-        """)
-
-        power, inverse = st.columns(2)
-
-        with power:
             st.write("""
-                ### Power Method
+                ## Summary Statistics
+                ##### The summary statistics provide an overview of the data distribution and convergence behavior.
             """)
-            st.bar_chart(results['power-method-iterations'], x_label='Matrix-Index', y_label='Iterations', height=500)
 
-        with inverse:
+            col1, col2 = st.columns([0.75,2.75])
+
+            with col1:
+                st.write(results.describe())
+            with col2:
+                st.write(results.head(8))
+
             st.write("""
-                ### Inverse Power Method
+                ## Dominant Eigenvalues
             """)
-            st.bar_chart(results['inverse-power-method-iterations'], x_label='Matrix-Index', y_label='Iterations', height=500)
+            st.scatter_chart(dominant, x_label='Matrix-Index', height=500)
 
-        row = results.iloc[int(results['power-method-iterations'].mean())]
-        matrix = np.array(eval(row['matrix']))
-        analyze_single_matrix(matrix)
+            st.write("""
+                ## Number of Iterations Required to Converge to the Dominant Eigenvalue
+                ##### The Inverse Power Method is a modification of the Power Method that gives faster convergence. It is not common to see the number of iterations required to converge for the Power Method to be less than the Inverse Power Method.
+            """)
+
+            power, inverse = st.columns(2)
+
+            with power:
+                st.write("""
+                    ### Power Method
+                """)
+                st.bar_chart(results['power-method-iterations'], x_label='Matrix-Index', y_label='Iterations', height=500)
+
+            with inverse:
+                st.write("""
+                    ### Inverse Power Method
+                """)
+                st.bar_chart(results['inverse-power-method-iterations'], x_label='Matrix-Index', y_label='Iterations', height=500)
+
+            row = results.iloc[int(results['power-method-iterations'].mean())]
+            matrix = np.array(eval(row['matrix']))
+            analyze_single_matrix(matrix, results)
+
+if __name__ == '__main__':
+    main()
